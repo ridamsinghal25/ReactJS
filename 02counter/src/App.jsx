040f3240a85d96cbafade690b467b7d6aa46e0db
Ridam-Singhal <manusinghal84@gmail.com>
2023-12-19 15:55:27 +0530

    created counter app

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  // let counter = 0;
+
+  let [counter, setCounter] = useState(0);
+
+  function addValue() {
+    if (counter >= 20) {
+      console.log("value greater than 20 not allowed");
+      return;
+    }
+    setCounter(counter + 1);
+  }
+
+  function deleteValue() {
+    if (counter <= 0) {
+      console.log("value less than 0 not allowed");
+      return;
+    }
+    setCounter(counter - 1);
+  }
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value {counter}</button>
+      <button onClick={deleteValue}>Delete Value {counter}</button>
+
+      <p>Footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;
