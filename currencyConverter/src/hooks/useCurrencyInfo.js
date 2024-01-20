9198b0beacf8de74f107aba01b92d2c7f62802a1
Ridam-Singhal <manusinghal84@gmail.com>
2024-01-20 16:48:32 +0530

    currency converter project

---
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+// hook to fetch information about the currencies exchange rate
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+  }, [currency]);
+
+  return data;
+}
+
+export default useCurrencyInfo;
