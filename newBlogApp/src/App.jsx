928b3ee900757a58445bfdeb1d0b58c3e9c6c742
ridamsinghal25 <ridamsinghal25@gmail.com>
2024-08-09 14:41:19 +0530

    add basic setup for new blog app

---
+++ newBlogApp/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold bg-slate-400 underline">
+        Hello world!
+      </h1>
+      <Button> Hello world </Button>
+    </>
+  );
+}
+
+export default App;
